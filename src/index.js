a2e86a7a9bcb8cdc1354c7d47e01c732b962edc5
stephenson-heritage <lstephenson@cegep-heritage.qc.ca>
2023-05-04 08:27:26 -0400

    some marker stuff

--- src/index.js
+++ src/index.js
@@ -34,30 +34,37 @@ var popupOffsets = {
 
 
 let initMap = () => {
+  tt.setProductInfo("test-demo", "0.0.1");
   map = tt.map({
     key: "NNnVPoPM9wVe2TNRA93zMuE6cKBdP2M0",
     container: "map",
     style: toxicStyle,
     center: [-75.737609, 45.455313],
     zoom: 12,
+    pitch: 10
   });
+
+
+
+
 
   var marker = new tt.Marker().setLngLat([-75.737609, 45.455313]).addTo(map);
 
 
 
-  marker.on('click', function (e) { console.log(e); });
 
-  var popup = new tt.Popup({ offset: popupOffsets, className: 'popup' }).setLngLat(marker.getLngLat())
+  var popup = new tt.Popup({ offset: popupOffsets, className: 'popup' })
     .setHTML("<h1>Hello I'm a Popup!</h1>")
     .addTo(map);
 
 
   marker.setPopup(popup);
 
-  marker.getElement().addEventListener('click', function () {
-    console.log('marker clicked');
+  marker.getElement().addEventListener('click', function (e) {
+    map.easeTo({ center: marker.getLngLat(), zoom: 14, duration: 1500 })
+
   });
 
+  marker.on('mousedown', function (e) { console.log(e, "mc"); });
 
 };
